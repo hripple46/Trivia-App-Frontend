4ef7057b348524c7cd817d3981cecabc025b4458
Henry Ripple <hripple46@gmail.com>
2023-08-22 17:53:12 -0500

    initalized react + tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-2xl text-red-600">
+        If this text is red, Tailwind is working.
+      </p>
+    </>
+  );
+}
+
+export default App;
