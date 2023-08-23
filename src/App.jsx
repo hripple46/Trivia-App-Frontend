94b118f7a966164c1a75c5b2388a3b889f5bd8bf
Henry Ripple <hripple46@gmail.com>
2023-08-23 18:15:01 -0500

    changed app layout for individual question styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,44 @@ import "./App.css";
 import Questions from "./Questions";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setQuestions(data.results);
+      });
+  }, []);
+
+  function decodeHtmlEntities(text) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = text;
+    return textArea.value;
+  }
   return (
     <>
-      <div className=" w-full h-full flex justify-center items-center">
-        <Questions />
+      <div className="w-full h-full flex justify-center items-center">
+        <ul className="max-w-sm ">
+          {questions.map((question) => {
+            return (
+              <li
+                //on click, set the background color to gray using utility classes
+                //hint: use the className prop
+                onClick={() => {
+                  console.log("clicked");
+                }}
+                key={question.question}
+              >
+                <Questions
+                  question={decodeHtmlEntities(question.question)}
+                ></Questions>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,38 +1,19 @@
 import { useState, useEffect } from "react";
 
-function Questions() {
-  const [questions, setQuestions] = useState([]);
-
-  useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=5")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setQuestions(data.results);
-      });
-  }, []);
-
-  function decodeHtmlEntities(text) {
-    const textArea = document.createElement("textarea");
-    textArea.innerHTML = text;
-    return textArea.value;
-  }
-
+function Questions({ question }) {
+  const [active, setActive] = useState(false);
   return (
     <>
-      <ul className="max-w-sm ">
-        {questions.map((question) => {
-          return (
-            <li
-              className="border-2 border-gray-400 rounded-md m-2 p-2"
-              key={question.question}
-            >
-              <div>{decodeHtmlEntities(question.question)}</div>
-            </li>
-          );
-        })}
-      </ul>
+      <div
+        className={`m-2 p-2 rounded-md ${
+          active ? "border-blue-400 border-2" : "border-gray-400 border-2"
+        }`}
+        onClick={() => {
+          setActive(true);
+        }}
+      >
+        {question}
+      </div>
     </>
   );
 }
