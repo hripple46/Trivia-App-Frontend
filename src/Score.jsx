44fbdf71992eb5802234e81db3cc1b6dd87cd6ce
Henry Ripple <hripple46@gmail.com>
2023-08-31 15:04:35 -0500

    added responsiveness to results

--- src/Score.jsx
+++ src/Score.jsx
@@ -26,13 +26,18 @@ export default function Questions({ score, results }) {
     <div
       className={
         showScore
-          ? `md:w-1/2 md:h-1/3 bg-blue-500/75 rounded-md text-white flex flex-col items-center justify-center fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`
+          ? `md:w-1/2 md:h-1/3 w-3/4 h-1/4 bg-blue-500/75 rounded-md text-white flex flex-col items-center sm:justify-center justify-between fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`
           : `hidden`
       }
     >
       {" "}
       <h1 className="text-2xl">{score} of 5</h1>
-      <button onClick={() => shareScore()}>Share Score</button>
+      <button
+        className="border-2 border-white p-2 rounded-md"
+        onClick={() => shareScore()}
+      >
+        Share Score
+      </button>
       <button onClick={() => setShowScore(false)}>Hide Score</button>
     </div>
   );
